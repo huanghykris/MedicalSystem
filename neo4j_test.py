4b5b6f51e0c6e502caa437edf91e0391a3c69b21
huanghykris <huanghy0820@163.com>
2025-03-31 19:15:55 +0800

    remove

--- neo4j_test.py
+++ neo4j_test.py
@@ -2,6 +2,6 @@ from py2neo import Graph,Subgraph
 from py2neo import Node,Relationship,Path
 
 # 连接数据库
-graph = Graph('bolt://localhost:7687', auth=('neo4j', '123456'))
+graph = Graph('bolt://localhost:7687', auth=('neo4j', '12345678'))
 
 graph.delete_all()
